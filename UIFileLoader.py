fe879c63d6bd3e0da0eae6dd3001d61777b90ae5
SiddharthaOm <78184131+SiddharthaOm@users.noreply.github.com>
2023-07-19 14:21:49 -0400

    Main Menu Mostly Working
    Added in Tab Functionality + Button Tool Loading

--- UIFileLoader.py
+++ UIFileLoader.py
@@ -6,101 +6,54 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('Hearts Of Iron 4 Arsenal.ui', self)
 
+        self.tools = {}  # Dictionary to store open tools
+
         self.tabWidget.clear()  # Clear any existing tabs
 
-        # Connect button signal to slot/function
-        self.CountryCreatorButton.clicked.connect(lambda: self.buttonClicked(self.CountryCreatorButton))
-        self.FocusTreeDesignerButton_2.clicked.connect(lambda: self.buttonClicked(self.FocusTreeDesignerButton_2))
-        self.LocalizationButton.clicked.connect(lambda: self.buttonClicked(self.LocalizationButton))
-        self.MapDesignerButton.clicked.connect(lambda: self.buttonClicked(self.MapDesignerButton))
-        self.TechTreeDesignerButton.clicked.connect(lambda: self.buttonClicked(self.TechTreeDesignerButton))
-        self.DataStructureFunctionsButton.clicked.connect(lambda: self.buttonClicked(self.DataStructureFunctionsButton))
-        self.UIDesignerButton.clicked.connect(lambda: self.buttonClicked(self.UIDesignerButton))
-        self.PeaceConferenceDesignerButton.clicked.connect(lambda: self.buttonClicked(self.PeaceConferenceDesignerButton))
-        self.AchievementDesignerButton.clicked.connect(lambda: self.buttonClicked(self.AchievementDesignerButton))
-        self.AIButton.clicked.connect(lambda: self.buttonClicked(self.AIButton))
-        self.PluginsButton.clicked.connect(lambda: self.buttonClicked(self.PluginsButton))
-        self.tabWidget.tabCloseRequested.connect(self.tabCloseRequested)
+        # Connect button signals to slots/functions
+        self.CountryCreatorButton.clicked.connect(lambda: self.openTool(self.CountryCreatorButton))
+        self.FocusTreeDesignerButton_2.clicked.connect(lambda: self.openTool(self.FocusTreeDesignerButton_2))
+        self.LocalizationButton.clicked.connect(lambda: self.openTool(self.LocalizationButton))
+        self.MapDesignerButton.clicked.connect(lambda: self.openTool(self.MapDesignerButton))
+        self.TechTreeDesignerButton.clicked.connect(lambda: self.openTool(self.TechTreeDesignerButton))
+        self.DataStructureFunctionsButton.clicked.connect(lambda: self.openTool(self.DataStructureFunctionsButton))
+        self.UIDesignerButton.clicked.connect(lambda: self.openTool(self.UIDesignerButton))
+        self.PeaceConferenceDesignerButton.clicked.connect(lambda: self.openTool(self.PeaceConferenceDesignerButton))
+        self.AchievementDesignerButton.clicked.connect(lambda: self.openTool(self.AchievementDesignerButton))
+        self.AIButton.clicked.connect(lambda: self.openTool(self.AIButton))
+        self.PluginsButton.clicked.connect(lambda: self.openTool(self.PluginsButton))
 
-    def buttonClicked(self, button):
-        button_text = button.text()
+        self.tabWidget.tabCloseRequested.connect(self.closeTool)
 
-        tab_index = self.tabWidget.addTab(QtWidgets.QWidget(), button_text)
-        self.tabWidget.setCurrentIndex(tab_index)
+    def openTool(self, button):
+        tool_name = button.text()
+        print("Loading tool:", tool_name)
 
-        if button is self.CountryCreatorButton:
-            self.countryCreatorClicked()
-        elif button is self.FocusTreeDesignerButton_2:
-            self.focusTreeDesignerClicked()
-        elif button is self.LocalizationButton:
-            self.localizationToolsClicked()
-        elif button is self.MapDesignerButton:
-            self.mapDesignerClicked()
-        elif button is self.TechTreeDesignerButton:
-            self.techTreeDesignerClicked()
-        elif button is self.DataStructureFunctionsButton:
-            self.dataStructureFunctionsClicked()
-        elif button is self.UIDesignerButton:
-            self.uiDesignerClicked()
-        elif button is self.PeaceConferenceDesignerButton:
-            self.peaceConferenceDesignerClicked()
-        elif button is self.AchievementDesignerButton:
-            self.achievementsDesignerClicked()
-        elif button is self.AIButton:
-            self.aiToolsClicked()
-        elif button is self.PluginsButton:
-            self.pluginsClicked()
+        # Check if the tool is already open
+        if tool_name in self.tools:
+            # If already open, select its tab
+            index = self.tabWidget.indexOf(self.tools[tool_name])
+            self.tabWidget.setCurrentIndex(index)
+        else:
+            # If not open, create a new tab and add the tool
+            tool_widget = QtWidgets.QWidget()  # Create a new instance of QWidget
+            tab_index = self.tabWidget.addTab(tool_widget, tool_name)
+            self.tools[tool_name] = tool_widget
+            self.tabWidget.setCurrentIndex(tab_index)
 
-    def countryCreatorClicked(self):
-        print("CountryCreatorButton clicked")
-
-
-    def focusTreeDesignerClicked(self):
-        print("FocusTreeDesignerButton_2 clicked")
-
-
-    def localizationToolsClicked(self):
-        print("LocalizationButton clicked")
-
-
-    def mapDesignerClicked(self):
-        print("MapDesignerButton clicked")
-
-    def techTreeDesignerClicked(self):
-        print("TechTreeDesignerButton clicked")
-
-
-    def dataStructureFunctionsClicked(self):
-        print("DataStructureFunctionsButton clicked")
-
-
-    def uiDesignerClicked(self):
-        print("UIDesignerButton clicked")
-
-
-    def peaceConferenceDesignerClicked(self):
-        print("PeaceConferenceDesignerButton clicked")
-
-
-    def achievementsDesignerClicked(self):
-        print("AchievementDesignerButton clicked")
-
-
-    def aiToolsClicked(self):
-        print("AIButton clicked")
-
-
-    def pluginsClicked(self):
-        print("PluginsButton clicked")
-
-
-    def tabCloseRequested(self, index):
-        print(f"Closing tab {index}")
+    def closeTool(self, index):
+        tool_widget = self.tabWidget.widget(index)
+        tool_name = self.tabWidget.tabText(index)
+        print("Closing tool:", tool_name)
+        self.tools.pop(tool_name, None)  # Remove the tool from the dictionary
         self.tabWidget.removeTab(index)
 
-    #Load Program
-if __name__ == '__main__':
+
+def main():
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
     main.show()
     sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
