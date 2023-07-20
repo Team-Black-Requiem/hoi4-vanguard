229fb109ae5868976e7a4767b87231f0a380e8d4
SiddharthaOm <78184131+SiddharthaOm@users.noreply.github.com>
2023-07-19 20:49:54 -0400

    Got the Focus Tree Designer UI in

--- UIFileLoader.py
+++ UIFileLoader.py
@@ -1,6 +1,22 @@
 from PyQt6 import QtWidgets, uic
 import sys
 
+
+class FocusTreeTool(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        scroll_area = QtWidgets.QScrollArea()
+        scroll_area.setWidgetResizable(True)
+
+        tool_content = QtWidgets.QWidget()
+        uic.loadUi("FocusToolDesigner.ui", tool_content)
+
+        scroll_area.setWidget(tool_content)
+
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(scroll_area)
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
@@ -8,7 +24,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.tools = {}  # Dictionary to store open tools
 
-        self.tabWidget.clear()  # Clear any existing tabs
+        self.tabWidget.clear()
 
         # Connect button signals to slots/functions
         self.CountryCreatorButton.clicked.connect(lambda: self.openTool(self.CountryCreatorButton))
@@ -40,6 +56,18 @@ class MainWindow(QtWidgets.QMainWindow):
             tab_index = self.tabWidget.addTab(tool_widget, tool_name)
             self.tools[tool_name] = tool_widget
             self.tabWidget.setCurrentIndex(tab_index)
+
+            if tool_name == "Focus Tree Designer":
+                # Create an instance of the FocusTreeTool widget
+                focus_tree_tool = FocusTreeTool(tool_widget)
+                layout = QtWidgets.QVBoxLayout(tool_widget)
+                layout.addWidget(focus_tree_tool)
+                tool_widget.setLayout(layout)
+
+                # Load FocusTreeTool.ui into the Viewport
+                focus_tree_tool_ui = uic.loadUi("FocusToolDesigner.ui")
+                self.Viewport.setLayout(QtWidgets.QVBoxLayout())
+                self.Viewport.layout().addWidget(focus_tree_tool_ui)
 
     def closeTool(self, index):
         tool_widget = self.tabWidget.widget(index)
