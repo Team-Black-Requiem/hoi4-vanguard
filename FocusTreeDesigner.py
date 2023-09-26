57d45f9566dcab64410219ed8305ab65f3616513
SiddharthaOm <78184131+SiddharthaOm@users.noreply.github.com>
2023-09-26 00:48:32 -0400

    Node Creation Testing
    Creates nodes but controls are all screwed up

--- FocusTreeDesigner.py
+++ FocusTreeDesigner.py
@@ -1,6 +1,6 @@
-from PyQt6.QtWidgets import QWidget, QGraphicsView, QGraphicsScene, QVBoxLayout
-from PyQt6.QtCore import Qt, QPointF, QLineF
-from PyQt6.QtGui import QPen, QColor
+from PyQt6.QtWidgets import QWidget, QGraphicsView, QGraphicsScene, QVBoxLayout, QGraphicsItem
+from PyQt6.QtCore import Qt, QPointF, QLineF, QRectF
+from PyQt6.QtGui import QPen, QColor, QPainter, QBrush 
 
 class FocusTreeTool(QWidget):
     def __init__(self):
@@ -11,11 +11,37 @@ class FocusTreeTool(QWidget):
         layout.addWidget(self.viewport)
         # Create and set up the QGraphicsView
         self.setLayout(layout)
-        
+
+    #### Begining of Focus Templates
+
+class defaultFocusNode(QGraphicsItem):
+    def __init__(self):
+        super().__init__()
+        self.rect = QRectF(-50, -25, 100, 50)  # Default size for the node
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)  # Correct flag name
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemSendsScenePositionChanges)  # Updated flag
+        self.setZValue(1)  # Ensure nodes appear on top of the grid
+
+    def boundingRect(self):
+        return self.rect
+
+    def paint(self, painter: QPainter, option, widget):
+        color = QColor(255, 255, 255)  # Default node color (white)
+        if self.isSelected():
+            color = QColor(0, 0, 255)  # Selected node color (blue)
+
+        pen = QPen(QColor(0, 0, 0))  # Node border color (black)
+        painter.setPen(pen)
+        painter.setBrush(QBrush(color))
+        painter.drawRoundedRect(self.rect, 5, 5)
+
+        ### End of Focus Templates
+
 class Viewport(QGraphicsView):
     def __init__(self):
         super().__init__()
-        
+
         self.setScene(QGraphicsScene())
         self.setViewportUpdateMode(QGraphicsView.ViewportUpdateMode.MinimalViewportUpdate)
         self.setSceneRect(-500, -500, 1000, 1000)
@@ -27,15 +53,15 @@ class Viewport(QGraphicsView):
         # Store the current grid origin
         self.grid_origin = QPointF(0, 0)
 
-        # Add a test item to the scene
-        self.addTestItem()
-
         # Variables to handle drag functionality
         self.last_mouse_pos = None
         self.setMouseTracking(True)
 
         # Store the zoom factor
         self.zoom_factor = 1.0
+
+        # Store created focus nodes
+        self.nodes = []
 
     def drawBackground(self, painter, rect):
         # Override the drawBackground method to draw the grid lines dynamically
@@ -54,10 +80,6 @@ class Viewport(QGraphicsView):
         painter.setPen(pen)
         painter.drawLines(lines)
 
-    def addTestItem(self):
-        item = self.scene().addRect(-100, -100, 200, 200, QPen(Qt.GlobalColor.green), Qt.GlobalColor.green)
-        item.setPos(QPointF(0, 0))
-
     def wheelEvent(self, event):
         # Enable zooming using the mouse wheel
         zoom_in = event.angleDelta().y() > 0
@@ -67,6 +89,9 @@ class Viewport(QGraphicsView):
 
     def mousePressEvent(self, event):
         self.last_mouse_pos = event.pos()
+
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.createFocusNode(event.pos())
 
     def mouseMoveEvent(self, event):
         if self.last_mouse_pos is not None:
@@ -83,3 +108,10 @@ class Viewport(QGraphicsView):
         self.grid_origin -= QPointF(dx, dy)
         self.setSceneRect(self.grid_origin.x() - 500, self.grid_origin.y() - 500, 1000, 1000)
         self.centerOn(self.grid_origin)
+
+    def createFocusNode(self, position):
+        # Create a new focus node and add it to the scene
+        focus_node = defaultFocusNode()
+        focus_node.setPos(self.mapToScene(position))  # Convert viewport coordinates to scene coordinates
+        self.scene().addItem(focus_node)
+        self.nodes.append(focus_node)  # Store the created node for further processing
